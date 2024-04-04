04b8fb69819ef277d8e3b0ab6051a51244b817e5
minesh.parekh <minesh.parekh@brainvire.com>
2024-04-04 21:21:09 +0530

    push notification code added

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,16 +1,44 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
 import { precacheAndRoute } from 'workbox-precaching';
 import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
 import { registerRoute } from 'workbox-routing';
 
+// Precache and route all assets defined in the __WB_MANIFEST variable
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Define caching strategies for specific routes
+
+// Network first strategy for requests to 'https://jsonplaceholder.typicode.com'
 registerRoute(
   ({ url }) => url.origin === 'https://jsonplaceholder.typicode.com',
   new NetworkFirst()
 );
 
+// Stale-While-Revalidate strategy for JavaScript, CSS, and HTML files
 registerRoute(
   /\.(?:js|css|html)$/,
   new StaleWhileRevalidate()
 );
+
+// Event listener for push events
+self.addEventListener('push', event => {
+  const payload = event.data ? event.data.text() : 'Default push notification';
+
+  event.waitUntil(
+    self.registration.showNotification('Push Notification', {
+      body: payload,
+    })
+  );
+});
+
+// Event listener for notification click events
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+
+  // Add logic here to handle what happens when the user clicks the notification
+  // For example, you can open a specific URL
+  event.waitUntil(
+    clients.openWindow(process.env.PUBLIC_URL)
+  );
+});
